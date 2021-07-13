adb71d97d077aa0adc6e96a0a0b3946b7f3e321f
theol@magenic.com <theol@magenic.com>
2021-07-13 18:02:59 +0800

    reverting change

--- example.test.js
+++ example.test.js
@@ -17,7 +17,7 @@ describe("Training Page 2 Tests", () => {
         await page.type('#Password', '123')
         await page.click('#Login')
         const welcomeMessage = await page.$eval('#WelcomeMessage', el => el.innerText)
-        await expect(welcomeMessage).toEqual('Welcome Homes')
+        await expect(welcomeMessage).toEqual('Welcome Home')
     })
 
     it("Invalid Login", async () => {

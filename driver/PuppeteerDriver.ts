58a7b80a3c1a5711650fac6807e0b9c93662128c
KurtM@magenic.com <KurtM@magenic.com>
2021-08-02 16:00:47 +0800

    Refactored getDriver()

--- driver/PuppeteerDriver.ts
+++ driver/PuppeteerDriver.ts
@@ -1,42 +1,46 @@
-import { Page } from "puppeteer";
+import { Page, Browser } from "puppeteer";
 
 export default class PuppeteerDriver implements IDriver {
     private isHeadless = false; // should be pulled out from config file
-    private page?: Page = undefined;
+    private browser?: Browser = undefined;
+    private page?: Page | undefined = undefined;
 
-    getDriver = async () =>  {
-        if (this.page === undefined) {
-            const browser = await await require('puppeteer').launch({
+
+    getDriver = async (): Promise<IDriver> =>  {
+        if (this.browser === undefined) {
+            this.browser = await await require('puppeteer').launch({
                 product: "chrome",
                 headless: this.isHeadless,
                 args: ["--start-maximized"],
                 defaultViewport: null,
             });
-            this.page = (await browser.pages())[0];
+            if (this.page === undefined) {
+                this.page = (await this.browser!.pages())[0];
+            }
         }
         return this as unknown as IDriver;
     };
 
-    navigateToUrl = async (url: string) => {
+    navigateToUrl = async (url: string): Promise<IDriver> => {
         await this.page!.goto(url);
         return this as unknown as IDriver;
     };
 
-    close = async () => {
+    close = async (): Promise<void> => {
         await this.page!.close();
     }
 
-    scrollIntoView = async (selector: string) => {
+    scrollIntoView = async (selector: string): Promise<IDriver> => {
         await this.page!.waitForSelector(selector, {visible: true}) ? 
         await this.page!.$eval(selector, (elem) => elem.scrollIntoView()) : null;
         return this as unknown as IDriver;
     }
 
-    url = async () => {
+    url = async (): Promise<string> => {
         return await this.page!.url();
     }
 
-    sleep = async (ms: number) => {
+    sleep = async (ms: number): Promise<void> => {
         await this.page!.waitForTimeout(ms);
     }
 }

af76a1f7b7e02b39aedbfe05c062302286c53c0d
theol@magenic.com <theol@magenic.com>
2021-08-04 19:13:50 +0800

    reverting due to test failures

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -112,12 +112,14 @@ describe("Training Page 2 Tests", () => {
         const femaleRadioBtn = await page.$('#FemaleRadio');
 
         //Add validation before selected
-        const isFemaleBtnSelected = await (await femaleRadioBtn?.getProperty("checked"))?.jsonValue();
+        let isFemaleBtnSelected = await (await femaleRadioBtn?.getProperty("checked"))?.jsonValue();
         
         if(!isFemaleBtnSelected)
         {
             await femaleRadioBtn?.click();
         }
+
+        isFemaleBtnSelected = await (await femaleRadioBtn?.getProperty("checked"))?.jsonValue();
         expect(isFemaleBtnSelected).toEqual(true);
     })
 })
